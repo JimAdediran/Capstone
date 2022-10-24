11cfab51132925e13c672e8a390a811fe364caae
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-10-24 14:36:04 -0400

    updated api function mark_shipment_not_food to calculate timedelta for current date minus date received for contribution

--- backend/contribution/views.py
+++ backend/contribution/views.py
@@ -5,7 +5,7 @@ from .models import Contribution
 from .serializers import ContributionSerializer
 from rest_framework import status
 from rest_framework.response import Response
-from datetime import date
+from datetime import date, datetime, timedelta
 
 # Create your views here.
 
@@ -21,7 +21,11 @@ def get_contribution(request):
 def mark_shipment_not_food(request, contribution_type, food):
     contributions = Contribution.objects.filter(contribution_type!=food)
     serializer = ContributionSerializer(contributions, many=True)
-    delta = date - Contribution.date_received
+    str_d1= datetime.now()
+    str_d2= Contribution.date_received
+    d1=datetime.strptime(str_d1, "%Y/%m/%d")
+    d2=datetime.strptime(str_d2, "%Y/%m/%d")
+    delta= d1-d2
     if delta.days >= 90:
         return Contribution.marked_for_shipment == True 
     return Response(serializer.data, status=status.HTTP_200_OK)
