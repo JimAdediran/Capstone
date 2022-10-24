648d3481d59001df49b7f9b6ad700ca2cac963f3
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-10-24 16:55:51 -0400

    mark_shipment_not_food function is able to calculate delta but still returning an error

--- backend/contribution/urls.py
+++ backend/contribution/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from contribution import views
 
 urlpatterns = [
-    path('<str:contribution_type>/<str:food>/', views.mark_shipment_not_food),
+    path('<str:products>/', views.mark_shipment_not_food),
     path('get/', views.get_contribution)
 ]
--- backend/contribution/views.py
+++ backend/contribution/views.py
@@ -16,19 +16,44 @@ def get_contribution(request):
     serializer = ContributionSerializer(contributions, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def mark_shipment_not_food(request, contribution_type, food):
+def mark_shipment_not_food(request, products):
+    contributions = Contribution.objects.filter(contribution_type=products)
+    serializer = ContributionSerializer(contributions, many=True)
+    current_time = datetime.today()
+    for contribution in contributions:
+        str_d1= current_time
+        str_d2= Contribution.date_received
+        d1= datetime.strptime(str(str_d1), "%Y-%m-%d %H:%M:%S.%f")
+        d2= datetime.strptime(str(str_d2), "%Y-%m-%d %H:%M:%S.%f")
+        delta= d1-d2
+        if delta.days >= 90:
+            return Contribution.marked_for_shipment == True 
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def mark_shipment_not_food1(request, contribution_type, food):
     contributions = Contribution.objects.filter(contribution_type!=food)
     serializer = ContributionSerializer(contributions, many=True)
+    for contribution in contributions:
+        check_shipment_status()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+def check_shipment_status():
     str_d1= datetime.now()
     str_d2= Contribution.date_received
     d1=datetime.strptime(str_d1, "%Y/%m/%d")
     d2=datetime.strptime(str_d2, "%Y/%m/%d")
     delta= d1-d2
     if delta.days >= 90:
-        return Contribution.marked_for_shipment == True 
-    return Response(serializer.data, status=status.HTTP_200_OK)
+            return Contribution.marked_for_shipment == True
+ 
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
