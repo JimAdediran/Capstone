b253a378d74396e0644e54352ced4eda03ad6114
Jimmy Adediran <jim_adediran@yahoo.com>
2023-04-01 15:53:26 -0400

    created ChartsPage.css + created and styled heading text for ChartsPage.js

--- frontend/src/pages/ChartsPage/ChartsPage.js
+++ frontend/src/pages/ChartsPage/ChartsPage.js
@@ -1,11 +1,12 @@
 import Chart from "../../components/GoogleCharts/GoogleCharts"
 import Chartregion from "../../components/GoogleCharts/GoogleChartsRegion"
-
+import "./ChartsPage.css"
 
 const ChartsPage = () =>{
 
 return (
     <div className="charts">
+        <a>Contribution data charts:</a>
         <Chart />
         <Chartregion />
     </div>
